6686323379c054c3eb2542cac56a55a99e7da80d
Bri'Anna Tolliver <bri39annat@gmail.com>
2023-01-25 23:25:18 -0600

    added all files and folders from course

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+//values and variables
+let country = "United States of America";
+let continent = "North America";
+let population = 329.5;
+console.log(country);
+console.log(continent);
+console.log(population);
